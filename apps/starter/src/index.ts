0dc66b87f00d18786825564aab3b6bcfaddfa153
tfevre <tfevre@metadev3.com>
2023-07-07 17:10:32 +0200

    first commit

--- apps/starter/src/index.ts
+++ apps/starter/src/index.ts
@@ -1,24 +1,41 @@
+import { XummJsonTransaction } from "xumm-sdk/dist/src/types";
 import { sendPayment } from "./transactions";
 import { WALLET_1, WALLET_2 } from "./wallet";
-import { getClient } from "./xrpl-client";
+// import { getClient } from "./xrpl-client";
+import { getXummClient } from "./xumm-client";
 
-const client = getClient();
+const xummClient = getXummClient ();
 
-const main = async () => {
-  await client.connect();
-  console.log("Connected to XRPL");
+const main2 = async () => {
+  const payload:XummJsonTransaction = {
+    TransactionType: "Payment",
+    Destination: WALLET_2.address,
+  }
 
-  await sendPayment(
-    {
-      Destination: WALLET_2.address,
-      Amount: "10"
-    },
-    {
-      wallet: WALLET_1
-    }
-  );
-  
-  await client.disconnect();
+  const response = await xummClient.payload.create(payload);
+
+  console.log(response);
 }
 
-main()
+main2();
+
+// const client = getClient();
+
+// const main = async () => {
+//   await client.connect();
+//   console.log("Connected to XRPL");
+
+//   await sendPayment(
+//     {
+//       Destination: WALLET_2.address,
+//       Amount: "10"
+//     },
+//     {
+//       wallet: WALLET_1
+//     }
+//   );
+  
+//   await client.disconnect();
+// }
+
+// main()
