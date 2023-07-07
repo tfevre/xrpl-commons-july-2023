572eef06126642a9e9bf55c2e73cdb1de2c2172e
tfevre <tfevre@metadev3.com>
2023-07-07 12:51:51 +0200

    first commit

--- apps/gemwallet/src/components/mint-nft/mint-nft.tsx
+++ apps/gemwallet/src/components/mint-nft/mint-nft.tsx
@@ -1,7 +1,71 @@
-import { Text } from "@chakra-ui/react"
+import { Button, Flex, FormControl, FormLabel, Input, Stack, Text } from "@chakra-ui/react"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { ExampleFormState } from "./example.types"
+import { MintNFTRequest, mintNFT } from "@gemwallet/api"
+import { stringToHex } from "../../shared/helpers"
 
 export const MintNft = () => {
-  // Help, use the function stringToHex from shared/helpers for the NFT URI.
+  const { register, handleSubmit } = useForm<MintNFTRequest>()
 
-  return <Text>Mint NFT TODO</Text>
+  const submitHandler: SubmitHandler<ExampleFormState> = (values) => {
+    const uri:string = values.URI != undefined ? stringToHex(values.URI) : "";
+    const memoText: string = (document.getElementById('memo') as HTMLInputElement).value;
+    const memoData: string = memoText != undefined ? stringToHex(memoText) : "";
+    const payload = {
+      URI: uri,
+      flags: {
+        tfOnlyXRP: true,
+        tfTransferable: true
+      },
+      fee: "13",
+      transferFee: 3000, // 3%,
+      NFTokenTaxon: 0,
+      memos: [
+        {
+          memo: {
+            memoData: memoData
+          }
+        }
+      ]
+    };
+    
+    mintNFT(payload).then((response) => {
+      console.log("NFT ID: ", response.result?.NFTokenID);
+      console.log("Transaction Hash: ", response.result?.hash);
+    });
+  }
+
+  return (
+    <Stack spacing="6">
+      <Text fontWeight="bold" fontSize="2xl">
+        Mint NFT
+      </Text>
+
+
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <Stack direction="column" spacing={8}>
+          <FormControl id="uri" isRequired>
+            <Stack direction={{ base: "column" }}>
+              <FormLabel>URI</FormLabel>
+              <Input {...register("URI")} />
+            </Stack>
+          </FormControl>
+
+          <FormControl id="memo" isRequired>
+            <Stack direction={{ base: "column" }}>
+              <FormLabel>Memo</FormLabel>
+              <Input  />
+            </Stack>
+          </FormControl>
+          
+        </Stack>
+
+        <Flex direction="row-reverse">
+          <Button backgroundColor="cyan.100" mt="8" type="submit">
+            Submit
+          </Button>
+        </Flex>
+      </form>
+    </Stack>
+  )
 }
--- apps/gemwallet/src/components/send-payment/send-payment.tsx
+++ apps/gemwallet/src/components/send-payment/send-payment.tsx
@@ -1,8 +1,52 @@
-import { Text } from "@chakra-ui/react"
+import { Button, Flex, FormControl, FormLabel, Input, Stack, Text } from "@chakra-ui/react"
+import { SubmitHandler, useForm } from "react-hook-form"
+import { ExampleFormState } from "./example.types"
+import { SendPaymentRequest, sendPayment } from "@gemwallet/api"
 
 export const SendPayment = () => {
   // Hint, use SendPaymentRequest from "@gemwallet/api" to define the form state
-  // const { register, handleSubmit } = useForm<SendPaymentRequest>()
+  const { register, handleSubmit } = useForm<SendPaymentRequest>()
 
-  return <Text>Send Payment TODO</Text>
+  const submitHandler: SubmitHandler<ExampleFormState> = (values) => {
+    const payment = {
+      amount: values.amount, // In drops (1 XRP)
+      destination: values.destination,
+    };
+    sendPayment(payment).then((response) => {
+      console.log("Transaction Hash: ", response.result?.hash);
+    });
+  }
+
+  return (
+    <Stack spacing="6">
+      <Text fontWeight="bold" fontSize="2xl">
+        Send Payments
+      </Text>
+
+
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <Stack direction="column" spacing={8}>
+          <FormControl id="recipient" isRequired>
+            <Stack direction={{ base: "column" }}>
+              <FormLabel>Recipient</FormLabel>
+              <Input {...register("destination")} />
+            </Stack>
+          </FormControl>
+
+          <FormControl id="amount" isRequired>
+            <Stack direction={{ base: "column" }}>
+              <FormLabel>Amount</FormLabel>
+              <Input {...register("amount")} />
+            </Stack>
+          </FormControl>
+        </Stack>
+
+        <Flex direction="row-reverse">
+          <Button backgroundColor="cyan.100" mt="8" type="submit">
+            Submit
+          </Button>
+        </Flex>
+      </form>
+    </Stack>
+  )
 }
